41045694ac5615a308814f3525efff781ed018c7
Raphaël AMIARD <raph.amiard@gmail.com>
2012-03-05 12:25:36 +0100

    GCD test

---
+++ test/testfiles/13_gcd_clean.ml
@@ -0,0 +1,8 @@
+let rec gcd a b =
+    if      a = 0 then b
+    else if b = 0 then a
+    else if a > b then gcd b (a mod b)
+    else               gcd a (b mod a);;
+
+print_int (gcd 12344 24482);;
+print_newline ();;
