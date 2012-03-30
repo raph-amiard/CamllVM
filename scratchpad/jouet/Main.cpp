9dbf1befbbda75d56dfa116dfc15cdd2d2368e05
Geoffroy Chollon <un.moins.1.egal.0@gmail.com>
2012-03-30 08:52:11 +0200

    hacking to make a working test executable

--- scratchpad/jouet/Main.cpp
+++ scratchpad/jouet/Main.cpp
@@ -1,45 +1,174 @@
-//#include "mvm/VMKit.h"
-#include "mvm/VirtualMachine.h"
-#include "mvm/MethodInfo.h"
-#include "mvm/Threads/Locks.h"
-//#include "mvm/Threads/Thread.h"
+//===------------- Main.cpp - Simple execution of J3 ----------------------===//
+//
+//                          The VMKit project
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
 
-#define NI() fprintf(stderr, "Not implemented\n"); abort()
+#include <iostream>
+
+#include "MvmGC.h"
+#include "mvm/JIT.h"
+//#include "mvm/MethodInfo.h"
+#include "mvm/VirtualMachine.h"
+#include "mvm/Threads/Thread.h"
+#include "mvm/Threads/Locks.h"
+#include "MutatorThread.h"
+
+//#include "llvm/Support/CommandLine.h"
+#include "llvm/Support/ManagedStatic.h"
+
+using namespace mvm;
+
+#include "FrametablesExterns.inc"
 
 mvm::CompiledFrames* frametables[] = {
+    #include "FrametablesSymbols.inc"
   NULL
 };
 
 
-namespace jouetvm {
+#include <iomanip>
+#include <ostream>
+#include <string>
 
-    class JouetVM : public mvm::VirtualMachine {
-    public:
-	JouetVM(mvm::BumpPtrAllocator &alloc, mvm::CompiledFrames** frames) : mvm::VirtualMachine(alloc, frames) {}
-	~JouetVM(){ NI(); }
-	/*virtual mvm::gc** getReferent(mvm::gc*) { NI(); }
-	virtual void setReferent(mvm::gc*, mvm::gc*) { NI(); }
-	virtual bool enqueueReference(mvm::gc*) { NI(); }*/
-	virtual size_t getObjectSize(gc* object) { NI(); }
-	virtual void printMethod(mvm::FrameInfo* FI, word_t ip, word_t addr) { NI(); }
-	virtual void runApplication(int, char**) { NI(); }
+struct hexdump {
+    void const* data;
+    int len;
+    hexdump(void const* data, int len) : data(data), len(len) {}
+
+    template<class T>
+    hexdump(T const& v) : data(&v), len(sizeof v) {}
+
+    friend
+    std::ostream& operator<<(std::ostream& s, hexdump const& v) {
+	// don't change formatting for s
+	std::ostream out (s.rdbuf());
+	out << std::hex << std::setfill('0');
+	unsigned char const* pc = reinterpret_cast<unsigned char const*>(v.data);
+	std::string buf;
+	buf.reserve(17); // premature optimization
+	int i;
+	for (i = 0; i < v.len; ++i, ++pc) {
+	    if ((i % 16) == 0) {
+		if (i) {
+		    out << "  " << buf << '\n';
+		    buf.clear();
+		}
+		out << "  " << std::setw(4) << i << ' ';
+	    }
+	    out << ' ' << std::setw(2) << unsigned(*pc);
+	    buf += (0x20 <= *pc && *pc <= 0x7e) ? *pc : '.';
+	}
+	if (i % 16) {
+	    char const* spaces16x3 = "                                                ";
+	    out << &spaces16x3[3 * (i % 16)];
+	}
+	out << "  " << buf << '\n';
+	return s;
+    }
+};
+
+//#include "../../lib/Jouet/MyCore/Myjvm.h"
+//using namespace jouet;
+
+
+class OcamlHeader : public gcRoot {
+public:
+    int u;
+    int v;
+    void tracer(word_t closure) {
+	std::cout << "OcamlHeader::tracer" << std::endl;
+	return;
+    }
+
+    OcamlHeader *omalloc(uint32_t sz) {
+	OcamlHeader *res = NULL;
+	res = (OcamlHeader *) gcmalloc(sz, this->getVirtualTable());
+	return res;
+    }
+};
+
+class OcamlThread : public mvm::MutatorThread {
+public:
+    OcamlThread(mvm::VirtualMachine *isolate) : MutatorThread() {
+	MyVM = isolate;
+	
+    };
+};
+
+
+class Ovm : public mvm::VirtualMachine {
+public:
+    Ovm(mvm::BumpPtrAllocator &Alloc, mvm::CompiledFrames** frames) :
+	mvm::VirtualMachine(Alloc, frames) { std::cout << "Ovm created" << std::endl; };
+
+    ~Ovm() {};
+
+    static void my_main_function(OcamlThread *torg) { 
+	std::cout << ">>> my_main_function; TotalMemory=" << Collector::getTotalMemory() << std::endl;
+
+
+	while (1) {
+	    std::cout << "??? my_main_function; TotalMemory=" << Collector::getTotalMemory()
+		      << std::endl;
+	    Collector::collect();
+
+	    OcamlHeader patron = OcamlHeader();
+	    OcamlHeader *x = NULL;
+	    //truc((void**) &x); //llvm_gcroot(x, 0);	
+	    x = patron.omalloc(32);
+	    std::cerr << "x~32 @" << x << ":\n" << hexdump(x, 32);
+	    
+	    OcamlHeader *y = NULL;
+	    //truc((void**) &y); //llvm_gcroot(y, 0);
+	    y = patron.omalloc(32);
+	    //y->header = 7;
+	    y->u = 3;
+	    y->v = std::numeric_limits<int>::max();
+	    std::cerr << "y~32 @"<< y << ":\n" << hexdump(y, 32);
+	    //break;
+	    sleep(2);
+	};
+
+	std::cout << "<<< my_main_function; TotalMemory=" << Collector::getTotalMemory() << std::endl;
+	return;
     };
 
-}
+    void startCollection() {
+	std::cout << "startCollection" << std::endl;
+    }
 
-using namespace jouetvm;
+    void runApplication(int argc, char** argv) {
+	std::cout << "runApplication" << std::endl;
+	mainThread = new OcamlThread(this);
+	std::cout << "starting mainThread" << std::endl;
+	mainThread->start((void (*)(mvm::Thread*)) my_main_function);
+    };
 
-int main(int argc, char **argv) {
-    /*
-    mvm::BumpPtrAllocator *Allocator = new mvm::BumpPtrAllocator();
-    mvm::CompiledFrames *Frames = new mvm::CompiledFrames();
-    //mvm::VMKit* vmkit = new(Allocator, "VMKit") mvm::VMKit(Allocator);
+    size_t getObjectSize(gc* object) { return 0; };
+    void printMethod(mvm::FrameInfo* FI, word_t ip, word_t addr) { return; };
 
-    JouetVM(Allocator, &Frames);
-    */
+};
 
-    mvm::BumpPtrAllocator Allocator;
-    JouetVM *vm = new(Allocator, "VM") JouetVM(Allocator, frametables);
+int main(int argc, char **argv, char **envp) {
+    
+  llvm::llvm_shutdown_obj X;
+  
+  // Initialize base components.  
+  //MvmModule::initialise(argc, argv);
+  
+  Collector::initialise(argc, argv);
+  Collector::verbose = 1;
 
-    return 0;
+  mvm::BumpPtrAllocator Allocator;
+  Ovm *i = new(Allocator, "i") Ovm(Allocator, frametables);
+  i->runApplication(argc, argv);
+  i->waitForExit();
+  i->~Ovm();
+  System::Exit(0);
+
+  return 0;
 }
